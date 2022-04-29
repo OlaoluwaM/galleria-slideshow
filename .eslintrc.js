ad0ed5f4a70a0367ba95013f7a67cb8c00f41c84
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2022-04-29 12:20:25 +0100

    feat: Project init

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  root: true,
+
+  env: {
+    browser: true,
+    es2021: true,
+  },
+
+  extends: [
+    'airbnb',
+    'react-app',
+    'react-app/jest',
+    'plugin:react/recommended',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+
+  settings: {
+    react: { version: 'detect' },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-console': 0,
+    'import/order': 0,
+
+    'consistent-return': 'warn',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+
+    'import/extensions': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import { StrictMode } from 'react';
+
+import './index.css';
+
+const UI_TREE_ROOT = document.getElementById('root')!;
+const root = ReactDOM.createRoot(UI_TREE_ROOT);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
